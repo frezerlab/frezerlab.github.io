978f5e4a5449d61fe9ef78c3604483ac0b03d66c
FrezerTop <39198630+FrezerTop@users.noreply.github.com>
2018-07-11 21:01:16 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-var nav = $('nav'),
-  posTop = nav.offset().top;
-
-$(document).on('scroll', function(){
-  nav[$(this).scrollTop() >= posTop ? 'addClass' : 'removeClass']('fixed');
-  $('.fixed .spreads').height(600-$(this).scrollTop());
-});
 
 //Блокирование масштаба
 window.addEventListener("mousewheel", function(e) {
@@ -13,3 +6,68 @@ window.addEventListener("mousewheel", function(e) {
     return false;
   }
 });
+
+//Tabs
+$(document).ready(function(){
+  $('.tabs').tabs();
+});
+
+//Tooltips
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
+
+
+
+$(".kek").click(function() {
+  //действия
+
+
+  // находим необходимый селектор
+  var btn = document.getElementById('hw');
+  var bio = document.getElementById('g');
+
+  // устанавливаем запрос
+  var request = new XMLHttpRequest();
+
+  // отслеживаем запрос
+  request.onreadystatechange = function() {
+    // проверяем вернулись запрошенные данные
+    if(request.readyState === 4) {
+      // добавляем рамку
+      bio.style.border = '1px solid #e8e8e8';
+      // проверяем успешен ли запрос
+      if(request.status === 200) {
+        // обнавляем элемент HTML
+        bio.innerHTML = request.responseText;
+      } else {
+        // иначе выводим сообщение об ошибке
+        bio.innerHTML = 'Произошла ошибка при запросе: ' +  request.status + ' ' + request.statusText;
+      }
+    }
+  }
+
+  // определяем тип запроса
+  request.open('Get', 'http://code.exabyte.ws/assets/files/biog.txt');
+
+  // регистрируем событие
+  btn.addEventListener('click', function() {
+    // скрываем кнопку
+    this.style.display = 'none';
+    // отправляем запрос
+    request.send();
+  });
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
