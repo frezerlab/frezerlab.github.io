deaf502cf9c2275d37d22e983f35bf3c3d11b6ae
FrezerTop <39198630+FrezerTop@users.noreply.github.com>
2018-07-17 22:35:58 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,51 +17,25 @@ $(document).ready(function(){
   $('.tooltipped').tooltip();
 });
 
+//Modals
+$(document).ready(function(){
+  $('.modal').modal();
+});
 
+//Material Box
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
 
-$(".kek").click(function() {
-  //действия
+//Убераем overflow у body
+$(".materialboxed").click(function() {
+  setTimeout(function () {
+    $("body").css({'overflow' : 'hidden'});
+  }, 1);
 
-
-  // находим необходимый селектор
-  var btn = document.getElementById('hw');
-  var bio = document.getElementById('g');
-
-  // устанавливаем запрос
-  var request = new XMLHttpRequest();
-
-  // отслеживаем запрос
-  request.onreadystatechange = function() {
-    // проверяем вернулись запрошенные данные
-    if(request.readyState === 4) {
-      // добавляем рамку
-      bio.style.border = '1px solid #e8e8e8';
-      // проверяем успешен ли запрос
-      if(request.status === 200) {
-        // обнавляем элемент HTML
-        bio.innerHTML = request.responseText;
-      } else {
-        // иначе выводим сообщение об ошибке
-        bio.innerHTML = 'Произошла ошибка при запросе: ' +  request.status + ' ' + request.statusText;
-      }
-    }
-  }
-
-  // определяем тип запроса
-  request.open('Get', 'http://code.exabyte.ws/assets/files/biog.txt');
-
-  // регистрируем событие
-  btn.addEventListener('click', function() {
-    // скрываем кнопку
-    this.style.display = 'none';
-    // отправляем запрос
-    request.send();
-  });
-
-
-
-
-
+  setTimeout(function () {
+    $("body").css({'overflow' : 'hidden'});
+  }, 250);
 });
 
 
@@ -69,5 +43,9 @@ $(".kek").click(function() {
 
 
 
+
+$(".kek").click(function() {
+
+});
 
 
