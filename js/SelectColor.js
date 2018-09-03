66a0c9ce93a083cb7cabc8f30aeeeaefcfb80bec
FrezerTop <39198630+FrezerTop@users.noreply.github.com>
2018-09-04 00:16:26 +0300

    Add files via upload

--- js/SelectColor.js
+++ js/SelectColor.js
@@ -2,6 +2,29 @@ curColor = "green";
 lastColor = "";
 ls = localStorage;
 
+HexColor = {
+  "red": "#f44336",
+  "pink": "#e91e63",
+  "purple": "#9c27b0",
+  "deep-purple": "#673ab7",
+  "indigo": "#3f51b5",
+  "blue": "#2196f3",
+  "light-blue": "#03a9f4",
+  "cyan": "#00bcd4",
+  "teal": "#009688",
+  "green": "#4caf50",
+  "light-green": "#8bc34a",
+  "lime": "#cddc39",
+  "yellow": "#ffeb3b",
+  "amber": "#ffc107",
+  "orange": "#ff9800",
+  "deep-orange": "#ff5722",
+  "brown": "#795548",
+  "grey": "#9e9e9e",
+  "blue-grey": "#607d8b"
+};
+
+
 $(document).ready(function() {
   curColor = ls.getItem('color') !== null? ls.getItem('color') : curColor;
   ReplaceColor();
@@ -24,6 +47,6 @@ function ReplaceColor(){
   $('*[data-color="' + curColor + '"]').addClass("pulse").removeClass("waves-effect");
   $('*[data-color="color"]').addClass(curColor).removeClass(lastColor);
   $('*[data-color="waves-color"]').removeClass("waves-" + lastColor).addClass("waves-" + curColor);
-
+  $('*[name="theme-color"]').attr('content', HexColor[curColor]);
 }
 
