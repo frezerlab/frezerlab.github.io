feaea1fe64906b2bda33e843b7413dc31fcb763f
FrezerTop <support@frezerlab.github.com>
2019-08-20 20:38:44 +0300

    20082019.3

--- js/main.js
+++ js/main.js
@@ -179,7 +179,8 @@ setInterval( function(){
 
 $('.backspace-input-btn').click(function () {
   if($('.backspace-input-btn').css('color') !== 'rgba(0, 0, 0, 0)'){
-    $(this).siblings('input').val('').focus();
+    $(this).siblings('input').val('');
+    $(this).siblings('input').focus();
   }
 });
 
